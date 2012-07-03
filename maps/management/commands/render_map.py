daa7e5c15e02f3030f4e195a18d221bf3d503703
Noah Seger <nosamanuel@gmail.com>
2012-07-02 22:33:00 -0500

    Add maps app with sample script to render globe

---
+++ maps/management/commands/render_map.py
@@ -0,0 +1,32 @@
+from kartograph import Kartograph
+
+
+def main():
+    k = Kartograph()
+    k.generate({
+        'proj': {
+            'id': 'ortho',
+            'lon0': -97.7428,
+            'lat0': 30.2669,
+        },
+        'layers': [{
+            'special': 'sea',
+            'styles': {
+                'fill': 'D0DDF0',
+            }
+        }, {
+            'id': 'countries',
+            'src': 'maps/fixtures/countries/ne_10m_admin_0_sovereignty.shp',
+            'styles': {
+                'fill': 'EEE9E6',
+            }
+        }],
+        'export': {
+            'width': 300,
+            'height': 300,
+        }
+    }, 'output.svg')
+
+
+if __name__ == '__main__':
+    main()
