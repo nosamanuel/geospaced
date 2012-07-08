ebbe9fa88439bd4b74dec4322e0b9c755b5c0c15
Noah Seger <nosamanuel@gmail.com>
2012-07-08 17:21:28 -0500

    Add Country model with shapefile importer

---
+++ countries/management/commands/import_countries.py
@@ -0,0 +1,62 @@
+import optparse
+import os
+import sys
+
+from django.core.management.base import LabelCommand
+from django.contrib.gis.utils.layermapping import LayerMapping, LayerMapError
+from django.template.defaultfilters import slugify
+
+from countries.models import Country
+
+
+class CountryLayerMapping(LayerMapping):
+    """
+    Custom layer mapping that sets a slug based on the country name
+    and sets the contested flag based on the country type.
+    """
+    mapping = {
+        'gdp': 'GDP_MD_EST',
+        'name': 'NAME',
+        'population': 'POP_EST',
+        'shape': 'Geometry',
+        'color': 'MAP_COLOR',
+        'iso2': 'ISO_A2',
+        'iso3': 'ISO_A3',
+    }
+
+    def __init__(self, shp_path, **kwargs):
+        kwargs.update(model=Country, data=shp_path, mapping=self.mapping)
+        super(CountryLayerMapping, self).__init__(**kwargs)
+
+    def feature_kwargs(self, feature):
+        kwargs = super(CountryLayerMapping, self).feature_kwargs(feature)
+
+        # Raise LayerMapError to ignore feature
+        if kwargs['iso2'] == '-99':
+            raise LayerMapError('Bad ISO2 "%(iso2)s" for "%(name)s"' % kwargs)
+
+        # Clean model fields
+        kwargs['slug'] = slugify(kwargs['name'])
+        if kwargs['gdp'] in (-99, 0):
+            kwargs['gdp'] = None
+        if kwargs['population'] in (-99, 0):
+            kwargs['population'] = None
+
+        return kwargs
+
+
+class Command(LabelCommand):
+    help = "Imports countries from the Natural Earth country shapefiles"
+    label = 'shp_path'
+    option_list = LabelCommand.option_list + (
+        optparse.make_option('--purge', action='store_true', dest='purge'),
+    )
+
+    def handle_label(self, label, **options):
+        if options.get('purge'):
+            sys.stdout.write('deleting all countries...')
+            Country.objects.all().delete()
+
+        shp_path = os.path.abspath(label)
+        mapping = CountryLayerMapping(shp_path, encoding='iso-8859-1')
+        mapping.save(strict=False, silent=False)
--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.conf import settings
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    objects = models.GeoManager()
+
+    # Facts
+    gdp = models.IntegerField(null=True)
+    languages = models.TextField(null=True)
+    name = models.CharField(max_length=256)
+    population = models.IntegerField(null=True)
+    shape = models.MultiPolygonField()
+
+    # Metadata
+    color = models.IntegerField()
+    contested = models.BooleanField(default=False)
+    iso2 = models.CharField(max_length=2)
+    iso3 = models.CharField(max_length=3)
+    slug = models.SlugField()
+
+    class Meta:
+        db_table = 'country'
+        verbose_name_plural = 'countries'
+
+    def __unicode__(self):
+        return self.name
+
+    @property
+    def flag_url(self):
+        return '%sflags/%s.svg' % (settings.STATIC_URL, self.iso2)
+
